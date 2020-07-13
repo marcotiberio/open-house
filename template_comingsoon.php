2b7d56f6b95213f8a668f5b024a91ff409d3589b
Marco <marco@defrostudio.eu>
2020-07-13 20:25:13 +0200

    coming soon template

---
+++ template_comingsoon.php
@@ -0,0 +1,16 @@
+<?php 
+	/* Template Name: Coming Soon */
+?>
+
+<?php get_header(); ?>
+
+<section class="background_comingsoon">
+
+    <img src="https://open-house.architektur.tu-berlin.de/wp-content/uploads/2020/07/IfA-Web_Placeholder.png" alt="TU Berlin IfA Logo">
+
+</section>
+
+
+<?php
+get_sidebar();
+get_footer();
