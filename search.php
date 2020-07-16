ac3cf19b33e5574c83b39a12b44e4be770e26a81
Marco <marco@defrostudio.eu>
2020-07-16 10:30:37 +0200

    no page header search

--- search.php
+++ search.php
@@ -14,15 +14,6 @@ get_header();
 
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'tu-berlin' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
-
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
